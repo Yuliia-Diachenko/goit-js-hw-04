c195dd28e955c32314893c0d45c51c41c4b484e8
Yuliia <123005661+Yuliia-Diachenko@users.noreply.github.com>
2024-01-22 20:12:32 +0200

    object-hw

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,28 @@
-function getElementWidth(content, padding, border) {  
-const boxSize=parseFloat(content)+parseFloat(padding)*2+parseFloat(border)*2;
-return boxSize;
+const profile = {
+    
+    username: "Jacob",
+    playTime: 300,
 
-}
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+    changeUsername(newName) {   
+    
+	profile.username = newName;
+},
+    updatePlayTime(hours) {
+    
+    profile.playTime = profile.playTime + hours;
+},
+    getInfo() {
+    
+    return `${profile.username} has ${profile.playTime} active hours!`;
+},
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
+
+
